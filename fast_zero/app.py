b24a71c9a30db01f2220c1d55ec3a75a4b6be512
Francisco Procópio <francisco.procopio@enacom.com.br>
2024-06-17 16:53:30 -0300

    creation of schemas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Hello, World!'}
+    return {'message': 'Hello, world!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK)
+def read_html():
+    return """
+    <head>
+        <title>Olá, Mundo</title>
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+    </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,19 @@ def test_read_root_retorna_ok_e_hello_world():
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Hello, World!'}
+    assert response.json() == {'message': 'Hello, world!'}
+
+
+def test_read_html_retorna_ola_mundo_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.text == '''
+    <head>
+        <title>Olá, Mundo</title>
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+    </body>
+    </html>'''
